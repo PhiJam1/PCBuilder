9bb8a2dd792dee78e923e1ff836a6b497ca5d8c9
Philip <Philipjames2004@gmail.com>
2023-12-20 23:12:10 -0600

    cleaned up some of the js-css organization

--- frontend/src/components/HomepageDescriptions.js
+++ frontend/src/components/HomepageDescriptions.js
@@ -1,29 +1,36 @@
 import Badge from 'react-bootstrap/Badge';
+import './../pages/HomePage.css';
 
 export default function HomePageDescriptions() {
     return (
-        <div className="bottom-half"> 
-            <h1> <Badge bg="secondary">Once Upon a Time...</Badge>Once Upon a Time... </h1>
-            <p>
-                When gamers, causal or committed, wanted the latest and greatest in personal computing technologies, 
-                they'd have the expensive source the parts themselves, research what works with what, and make guesses as to what
-                the best tradeoffs are to keep from breaking the whole piggy bank.
-            </p>
-            <h1> Well not anymore. </h1>
-            <p>
-                Here at shafaath, we've already done the sourcing, and research, and know all the best tradeoffs for your 
-                specific needs. <b>We remove the guess work from PC building for your peace of mind.</b> And by peace of mind, we 
-                mean not having to worry about buying and assembling all the parts only to find your new PC not turning on. We
-                deliver the highest quality and guarantee any defective service will be fully refunded and fixed (right yu?). 
-            </p>
-            <h1> How Does This Work </h1>
-            <p>
-                First, you can design your dream PC in our design studio (make this a button). At each stage, select the part you
-                want and read up on the provided expert opinion. If you make an account with us, you can save your build and return
-                to it later. Once you submit a build, one of our nerds will get back to you in a few (timeframe yu) to confirm. Then,
-                in a few days, your PC will arrive at your door ready to go! If at any point, you're stuck with your build, just submit
-                a review form and one of our nerds will be in touch for free (yea?) consultation.
-            </p>
+        <div>
+            <div className='bottom-right'> 
+                <h1> <Badge bg="secondary">Once Upon a Time...</Badge>Once Upon a Time... </h1>
+                <p>
+                    When gamers, causal or committed, wanted the latest and greatest in personal computing technologies, 
+                    they'd have the expensive source the parts themselves, research what works with what, and make guesses as to what
+                    the best tradeoffs are to keep from breaking the whole piggy bank.
+                </p>
+            </div>
+            <div className='bottom-right'> 
+                <h1> Well not anymore. </h1>
+                <p>
+                    Here at shafaath, we've already done the sourcing, and research, and know all the best tradeoffs for your 
+                    specific needs. <b>We remove the guess work from PC building for your peace of mind.</b> And by peace of mind, we 
+                    mean not having to worry about buying and assembling all the parts only to find your new PC not turning on. We
+                    deliver the highest quality and guarantee any defective service will be fully refunded and fixed (right yu?). 
+                </p>
+            </div>
+            <div className='bottom-right'>
+                <h1> How Does This Work </h1>
+                <p>
+                    First, you can design your dream PC in our design studio (make this a button). At each stage, select the part you
+                    want and read up on the provided expert opinion. If you make an account with us, you can save your build and return
+                    to it later. Once you submit a build, one of our nerds will get back to you in a few (timeframe yu) to confirm. Then,
+                    in a few days, your PC will arrive at your door ready to go! If at any point, you're stuck with your build, just submit
+                    a review form and one of our nerds will be in touch for free (yea?) consultation.
+                </p>
+            </div>
         </div>
     );
 };
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,14 +3,20 @@ import  HomePageDescriptions  from '../components/HomepageDescriptions.js';
 import HomePageDSBTN from '../components/HomePageDSBTN.js';
 export const HomePage = () => {
     return ( // this stuff is JSX
-    <div className="homepage">
-        <div className='top-half'>
-            <HomePageLoginPrompt />
-            <br></br>
-            <HomePageDSBTN />
+    <div className='homepage-full'>
+        <div className="homepage">
+            <h1 style={{fontSize: '5rem'}}>This Is Going To Be</h1>
+            <h1 style={{fontSize: '7rem'}}>Awesome . . .</h1>
+            <div className='top-half'>
+                
+
+                <HomePageLoginPrompt />
+                <br></br>
+                <HomePageDSBTN />
+            </div>
         </div>
-        <div>
-            {/*<HomePageDescriptions />*/}
+        <div className="homepage2">
+            <HomePageDescriptions />
         </div>
     </div>
     
