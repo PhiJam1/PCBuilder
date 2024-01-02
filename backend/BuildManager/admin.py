923045b93b1a7736bf0c85903d6987ea9d2b355d
Philip <Philipjames2004@gmail.com>
2024-01-02 00:28:06 -0600

    added Build model

--- backend/BuildManager/admin.py
+++ backend/BuildManager/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Part
-
+from . models import Part
+from . models import Build
 # Register your models here.
 admin.site.register(Part)
+admin.site.register(Build)
--- backend/BuildManager/models.py
+++ backend/BuildManager/models.py
@@ -21,20 +21,26 @@ class Build(models.Model):
     def get_build_num():
         return random.randint(11, 1000)
     
-
-    buildNum = models.IntegerField(unique=True default=get_build_num)
+    # logistic information
+    buildNum = models.IntegerField(unique=True, default=get_build_num)
     status = models.CharField(max_length=200)
     phoneNumber = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
+    template = models.CharField(max_length=200)
+    problem_description = models.TextField()
     # parts
-    currCase = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currCPU = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currCPUCooler = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currMotherboard = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currMemory = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currStorage = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currGPU = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currPowerSupply = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    currOperatingSystem = models.ForeignKey(Part, on_delete=models.SET_NULL)
-    other = models.TextField();
-    otherCost = models.DecimalField(max_digits=10, decimal_places=2)
+    currCase = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='case')
+    currCPU = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='cpu')
+    currCPUCooler = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='cpu_cooler')
+    currMotherboard = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='motherboard')
+    currMemory = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='memory')
+    currStorage = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='storage')
+    currGPU = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='gpu')
+    currPowerSupply = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='power_supply')
+    currOperatingSystem = models.ForeignKey(Part, on_delete=models.SET_NULL, null=True, related_name='operating_system')
+    other = models.TextField()
+    otherCost = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    def get_total_cost(self):
+        total = self.currCase.cost + self.currCPU.cost + self.currCPUCooler + self.currMotherboard + self.currMemory + self.currGPU + self.currPowerSupply + self.currOperatingSystem + self.otherCost
+        return total
